0d50e17aad23a3bbb6a00654238215ca9af576b5
Nelson Fernandes <invoke.std@gmail.com>
2024-07-03 02:14:52 +0100

    Solução do Exercicio

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,32 @@ import "./index.css";
 
 // Cria os teus elementos usando React.createElement
 
-ReactDOM.createRoot(document.getElementById("root"))
-	.render
-	// Renderiza o teu componente aqui!
-	();
+const Cubo = React.createElement("div", {
+	className: "cubo",
+});
+
+const Title = React.createElement(
+	"h2",
+	{
+		className: "titulo",
+	},
+	"Hello Edit!"
+);
+
+const mainContainer = React.createElement(
+	"div",
+	{
+		class: "main-container",
+	},
+	Title,
+	Cubo,
+	Cubo,
+	Cubo,
+	Cubo,
+	Cubo,
+	Cubo
+);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(mainContainer);
